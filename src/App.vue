529cb6e971bd25347bda390cf342161f12dd1b21
Samuel <forfemi@gmail.com>
2023-10-18 12:08:16 +0100

    vue-calcultor-app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+<CalculatorPage></CalculatorPage>
+
+</template>
+
+<script>
+import CalculatorPage from './components/CalculatorPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorPage
+  }
+}
+</script>
+
+<style>
+
+</style>
